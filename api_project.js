9f98ccac8e4005f6a72c02018dab14d1837db94b
ark <flukycoder@gmail.com>
2020-12-24 20:59:53 +0530

    merge

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "title": "Custom apiDoc browser title",
+  "url": "https://warm-earth-10100.herokuapp.com/api/v1",
+  "name": "roastme",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-12-24T15:25:11.997Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
